0e19575e2f0e4ca06773011c360de21f46cb8fb5
Nick Teets <nickteets@gmail.com>
2017-10-12 08:55:48 -0600

    Puts together Loader css and adds logo

--- src/Loader/Loader.js
+++ src/Loader/Loader.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import logo from '../assets/imp-logo.svg';
 
 const Loader = () => {
   return (
-    <div>
-      Loading...
+    <div className="loader-div">
+      <p>Loading...</p>
+      <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
 };
